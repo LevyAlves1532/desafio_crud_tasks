bb8fc1ad45814e61f99fe509b144b5c152758aa5
Lêvy Alves <levy.pereiraA1532@gmail.com>
2026-03-27 17:17:05 -0300

    add script to generate CSV file with tasks and handle directory creation

---
+++ scripts/generate-file-task-csv.js
@@ -0,0 +1,32 @@
+import fs from 'node:fs/promises';
+
+let body = `title,description\n`;
+
+for (let x = 1;x<=100000;x++) {
+    body += `Task ${x},Description Task ${x}\n`;
+}
+
+const csvPath = new URL('../imports/tasks.csv', import.meta.url);
+const csvFolderPath = new URL('../imports', import.meta.url);
+
+validFolderOrCreate();
+
+function validFolderOrCreate() {
+    fs.readdir(csvFolderPath)
+        .then(() => createFileCSV())
+        .catch(() => {
+            fs.mkdir(csvFolderPath);
+
+            createFileCSV();
+        });
+}
+
+function createFileCSV() {
+    fs.writeFile(csvPath, body)
+        .then(() => {
+            console.info('Created file csv');
+        })
+        .catch((err) => {
+            console.error('Error: ' + err);
+        });
+}
